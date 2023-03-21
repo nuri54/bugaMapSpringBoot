d64e39873cf52396d724de707fdc0788a3a5dd89
Maximilian König <maximilian.koenig@mein.gmx>
2023-03-21 23:16:53 +0100

    Controller for user

---
+++ src/main/java/de/hhn/se/labswp/buga23test/controller/UserController.java
@@ -0,0 +1,32 @@
+package de.hhn.se.labswp.buga23test.controller;
+
+import de.hhn.se.labswp.buga23test.crudRepos.UserRepository;
+import de.hhn.se.labswp.buga23test.jpa.User;
+import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+
+  public UserController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  private final UserRepository userRepository;
+
+  @GetMapping("/users")
+  public List<User> getUsers() {
+    return (List<User>) userRepository.findAll();
+  }
+
+  @PostMapping("/users")
+  void addUser(@RequestBody User user) {
+    userRepository.save(user);
+  }
+
+}
