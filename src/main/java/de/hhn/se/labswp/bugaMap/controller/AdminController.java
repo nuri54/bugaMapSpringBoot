d461c1a74641db19ba6834295e3b7b0dfbce5a1a
nuri <hendennurullah@gmail.com>
2023-04-15 19:53:21 +0200

    fixed merge issues

--- src/main/java/de/hhn/se/labswp/bugaMap/controller/AdminController.java
+++ src/main/java/de/hhn/se/labswp/bugaMap/controller/AdminController.java
@@ -5,10 +5,12 @@ import de.hhn.se.labswp.bugaMap.jpa.Admin;
 import java.util.List;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(path = "/test")
 public class AdminController {
 
   public AdminController(AdminRepository adminRepository) {
@@ -21,7 +23,5 @@ public class AdminController {
   public List<Admin> getUsers() {
     return (List<Admin>) adminRepository.findAll();
   }
-
-
 
 }
