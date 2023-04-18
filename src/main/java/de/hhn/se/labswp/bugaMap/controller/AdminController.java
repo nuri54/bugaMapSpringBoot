758d74520a567828ce67cd316f1cad4b7f7070af
Maximilian König <maximilian.koenig@mein.gmx>
2023-04-18 23:41:31 +0200

    Response for admin/add when email is duplicated

--- src/main/java/de/hhn/se/labswp/bugaMap/controller/AdminController.java
+++ src/main/java/de/hhn/se/labswp/bugaMap/controller/AdminController.java
@@ -2,7 +2,9 @@ package de.hhn.se.labswp.bugaMap.controller;
 
 import de.hhn.se.labswp.bugaMap.crudRepos.AdminRepository;
 import de.hhn.se.labswp.bugaMap.jpa.Admin;
+import de.hhn.se.labswp.bugaMap.responses.DatabaseSaveResponse;
 import java.util.List;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -23,10 +25,17 @@ public class AdminController {
 
   private final AdminRepository adminRepository;
 
+  /**
+   * Standard request to get all admins.
+   * Notice: Passwords get blacken.
+   *
+   * @return All admins.
+   */
   @GetMapping("/list")
   public List<Admin> getAdmins() {
     List<Admin> admins = (List<Admin>) adminRepository.findAll();
 
+    // Hide passwords
     for (Admin admin :
         admins) {
       admin.setPassword("[HIDDEN]");
@@ -40,15 +49,26 @@ public class AdminController {
    * POST Request to save one admin.
    *
    * @param admin Admin which will be put into the database.
-   * @return
+   * @return response if saving to the database was successful or not.
    */
   @PostMapping(value = "/add")
-  public ResponseEntity<String> addAdmin(@RequestBody Admin admin) {
+  public ResponseEntity<DatabaseSaveResponse> addAdmin(@RequestBody Admin admin) {
     try {
       adminRepository.save(admin);
-      return ResponseEntity.ok("Admin saved");
+      return ResponseEntity.ok(new DatabaseSaveResponse(true, "Admin saved"));
     } catch (Exception e) {
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error saving admin");
+
+      // Return special message if mail address is already in use
+      if (e.getCause() instanceof ConstraintViolationException) {
+        if (e.getCause().getCause().getMessage().contains("Duplicate") &&
+            e.getCause().getCause().getMessage().contains("Emailadress")) {
+          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+              .body(new DatabaseSaveResponse(false, "Emailadress duplicated"));
+        }
+      }
+
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body(new DatabaseSaveResponse(false, e.getMessage()));
     }
   }
 
