d5c3f816e1b7512c2b27d8b8f776ee6a91e2d22f
Julian <juliansammet@gmail.com>
2023-04-21 00:56:01 +0200

    added CrossOrigin

--- src/main/java/de/hhn/se/labswp/bugaMap/security/auth/AuthenticationController.java
+++ src/main/java/de/hhn/se/labswp/bugaMap/security/auth/AuthenticationController.java
@@ -2,12 +2,10 @@ package de.hhn.se.labswp.bugaMap.security.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
