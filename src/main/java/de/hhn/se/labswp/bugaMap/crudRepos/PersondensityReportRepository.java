1ced28a33c68f895b42b9623dd3c279363181a89
Maximilian König <maximilian.koenig@mein.gmx>
2023-04-22 23:16:56 +0200

    added Persondensityreport

---
+++ src/main/java/de/hhn/se/labswp/bugaMap/crudRepos/PersondensityReportRepository.java
@@ -0,0 +1,9 @@
+package de.hhn.se.labswp.bugaMap.crudRepos;
+
+import de.hhn.se.labswp.bugaMap.jpa.Persondensityreport;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersondensityReportRepository
+    extends CrudRepository<Persondensityreport, Integer> {
+
+}
