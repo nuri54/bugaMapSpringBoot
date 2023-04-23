20f0613aca1a6d94f3f9c7d09a29b6b4262b2b15
Maximilian König <maximilian.koenig@mein.gmx>
2023-04-23 17:54:50 +0200

    refactored

--- src/main/java/de/hhn/se/labswp/bugaMap/security/auth/AuthenticationController.java
+++ src/main/java/de/hhn/se/labswp/bugaMap/security/auth/AuthenticationController.java
@@ -1,16 +1,10 @@
 package de.hhn.se.labswp.bugaMap.security.auth;
 
-import de.hhn.se.labswp.bugaMap.crudRepos.AdminRepository;
-import de.hhn.se.labswp.bugaMap.jpa.Admin;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
   private final AuthenticationService service;
-  private final AdminRepository adminRepository;
 
   @PostMapping("/register")
   public ResponseEntity<AuthenticationResponse> register(
