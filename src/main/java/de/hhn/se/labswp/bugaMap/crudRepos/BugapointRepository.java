3970fef2e0b87e8ef07f8987cc092d1230e9ed51
Maximilian König <maximilian.koenig@mein.gmx>
2023-04-26 18:58:43 +0200

    Added delete method in service und updated method

--- src/main/java/de/hhn/se/labswp/bugaMap/crudRepos/BugapointRepository.java
+++ src/main/java/de/hhn/se/labswp/bugaMap/crudRepos/BugapointRepository.java
@@ -1,8 +1,12 @@
 package de.hhn.se.labswp.bugaMap.crudRepos;
 
 import de.hhn.se.labswp.bugaMap.jpa.Bugapoint;
+import java.util.Collection;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 /**
@@ -10,5 +14,14 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface BugapointRepository extends CrudRepository<Bugapoint,Integer> {
+  @Transactional
+  @Modifying
+  @Query("""
+      update Bugapoint b set b.latitude = ?1, b.longitude = ?2, b.description = ?3, b.adminID = ?4
+      where b.id in ?5""")
+  int updateLatitudeAndLongitudeAndDescriptionAndAdminIDByIdIn(Double latitude, Double longitude,
+      String description, int adminID, int id);
 
+  @Override
+  void deleteById(Integer integer);
 }
