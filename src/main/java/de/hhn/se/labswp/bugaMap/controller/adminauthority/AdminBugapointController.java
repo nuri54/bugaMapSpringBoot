7bf22c481c92b8c9d4638edd79d4237ef5b14655
Maximilian König <maximilian.koenig@mein.gmx>
2023-04-28 16:09:43 +0200

    Created

---
+++ src/main/java/de/hhn/se/labswp/bugaMap/controller/adminauthority/AdminBugapointController.java
@@ -0,0 +1,16 @@
+package de.hhn.se.labswp.bugaMap.controller.adminauthority;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Mappings with Admin access for Admin table.
+ */
+@RestController()
+@RequestMapping("/admin")
+public class AdminBugapointController {
+
+
+
+
+}
