9f523841c0d2b1986046765eef726dfd59184b2c
Maximilian König <maximilian.koenig@mein.gmx>
2023-05-21 17:36:58 +0200

    path fix

--- src/main/java/de/hhn/se/labswp/bugamap/controller/AdminController.java
+++ src/main/java/de/hhn/se/labswp/bugamap/controller/AdminController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Mappings for Admin table.
  */
 @RestController()
-@RequestMapping("/api/v1/management")
+@RequestMapping("/api/v1/open/management")
 @CrossOrigin(origins = "http://localhost:4200")
 public class AdminController {
 
@@ -43,7 +43,7 @@ public class AdminController {
    */
   @GetMapping("/list")
   public List<Admin> getAdmins() {
-    List<Admin> admins = (List<Admin>) adminRepository.findAll();
+    List<Admin> admins = adminRepository.findAll();
 
     // Hide passwords
     for (Admin admin :
