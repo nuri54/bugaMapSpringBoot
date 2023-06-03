5d5202c01515a7fb7610a3e36695125059419f8b
Maximilian König <maximilian.koenig@mein.gmx>
2023-06-03 17:50:21 +0200

    Correct response

--- src/main/java/de/hhn/se/labswp/bugamap/controller/adminauthority/AdminApproveController.java
+++ src/main/java/de/hhn/se/labswp/bugamap/controller/adminauthority/AdminApproveController.java
@@ -2,6 +2,7 @@ package de.hhn.se.labswp.bugamap.controller.adminauthority;
 
 import de.hhn.se.labswp.bugamap.controller.adminauthority.requestbodies.ApproveRequest;
 import de.hhn.se.labswp.bugamap.controller.adminauthority.responses.ListToBeAcceptedResponse;
+import de.hhn.se.labswp.bugamap.responses.DatabaseSaveResponse;
 import de.hhn.se.labswp.bugamap.security.auth.roles.Role;
 import de.hhn.se.labswp.bugamap.crudrepos.AdminRepository;
 import de.hhn.se.labswp.bugamap.jpa.Admin;
@@ -9,6 +10,8 @@ import de.hhn.se.labswp.bugamap.security.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -17,49 +20,53 @@ import java.util.*;
 @RequestMapping(path = "/api/v1/admin")
 @RequiredArgsConstructor
 public class AdminApproveController {
-    private final static Logger logger = LogManager.getLogger(JwtService.class);
-    private final AdminRepository adminRepository;
 
-    @PutMapping("/approve")
-    public void approveUserWithEmail(
-            @RequestBody ApproveRequest userEmail) {
+  private final static Logger logger = LogManager.getLogger(JwtService.class);
+  private final AdminRepository adminRepository;
 
-        Optional<Admin> adminFound = adminRepository.findByEmailadress(userEmail.getEmail());
-        if (adminFound.isPresent()) {
-            Admin admin = adminFound.get();
-            admin.setRole(Role.MANAGER);
-            adminRepository.save(admin);
-            logger.info("Role for " + userEmail.getEmail() + " updated to Manager");
-        } else
-            logger.info("Update Role for User failed because User not found: " + userEmail.getEmail() + " set role to Manager");
+  @PutMapping("/approve")
+  public ResponseEntity<DatabaseSaveResponse> approveUserWithEmail(
+      @RequestBody ApproveRequest userEmail) {
 
+    Optional<Admin> adminFound = adminRepository.findByEmailadress(userEmail.getEmail());
+    if (adminFound.isPresent()) {
+      Admin admin = adminFound.get();
+      admin.setRole(Role.MANAGER);
+      adminRepository.save(admin);
+      logger.info("Role for " + userEmail.getEmail() + " updated to Manager");
+      return ResponseEntity.ok(new DatabaseSaveResponse(true, "Manager wurde approved"));
+    } else {
+      logger.info("Update Role for user failed because user not found: " + userEmail.getEmail()
+          + " set role to Manager");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new DatabaseSaveResponse(false, "Manager wurde nicht approved"));
+    }
+  }
+
+
+  @GetMapping("listNotAccepted")
+  public List<ListToBeAcceptedResponse> getAllToBeAcceptedUsers() {
+    List<Object[]> adminList = adminRepository.findAllToBeAcceptedUsers();
+    List<ListToBeAcceptedResponse> responseList = new ArrayList<>();
+
+    for (Object[] admin : adminList) {
+      String email = (String) admin[0];
+      String firstname = (String) admin[1];
+      String surname = (String) admin[2];
+      Role role = (Role) admin[3];
+      String rolename = role.name();
+
+      ListToBeAcceptedResponse response = ListToBeAcceptedResponse.builder()
+          .email(email)
+          .firstname(firstname)
+          .surname(surname)
+          .role(rolename)
+          .build();
+
+      responseList.add(response);
     }
 
+    return responseList;
 
-    @GetMapping("listNotAccepted")
-    public List<ListToBeAcceptedResponse> getAllToBeAcceptedUsers() {
-        List<Object[]> adminList = adminRepository.findAllToBeAcceptedUsers();
-        List<ListToBeAcceptedResponse> responseList = new ArrayList<>();
-
-        for (Object[] admin : adminList) {
-            String email = (String) admin[0];
-            String firstname = (String) admin[1];
-            String surname = (String) admin[2];
-            Role role = (Role) admin[3];
-            String rolename = role.name();
-
-            ListToBeAcceptedResponse response = ListToBeAcceptedResponse.builder()
-                    .email(email)
-                    .firstname(firstname)
-                    .surname(surname)
-                    .role(rolename)
-                    .build();
-
-            responseList.add(response);
-        }
-
-        return responseList;
-
-    }
+  }
 }
 
