cd450f26039ff1212b1fe0d7e2a0a79f52478535
Maximilian König <maximilian.koenig@mein.gmx>
2023-06-03 13:10:18 +0200

    just base test

--- src/test/java/de/hhn/se/labswp/bugamap/controller/BaseTest.java
+++ src/test/java/de/hhn/se/labswp/bugamap/controller/BaseTest.java
@@ -1,9 +1,10 @@
-package de.hhn.se.labswp.bugamap.controller.config;
+package de.hhn.se.labswp.bugamap.controller;
 
 import de.hhn.se.labswp.bugamap.crudrepos.AdminRepository;
 import de.hhn.se.labswp.bugamap.crudrepos.BugapointRepository;
 import de.hhn.se.labswp.bugamap.crudrepos.ParkRepository;
 import de.hhn.se.labswp.bugamap.jpa.Admin;
+import de.hhn.se.labswp.bugamap.jpa.Bugapoint;
 import de.hhn.se.labswp.bugamap.jpa.Park;
 import de.hhn.se.labswp.bugamap.security.auth.SecurityTestConfig;
 import de.hhn.se.labswp.bugamap.security.auth.roles.Role;
@@ -31,6 +32,12 @@ public class BaseTest {
   @Autowired
   private PasswordEncoder passwordEncoder;
 
+  @Autowired
+  private BugapointRepository bugapointRepository;
+
+  /**
+   * Loads in dummy data.
+   */
   @BeforeEach
   public void setup() {
 
@@ -59,9 +66,36 @@ public class BaseTest {
     superAdmin.setRole(Role.ADMIN);
 
     adminRepository.save(superAdmin);
+
+    Bugapoint haupteingangLuisenpark = Bugapoint.builder()
+        .id(1)
+        .title("Haupteingang Luisenpark")
+        .adminID(1)
+        .latitude(49.47935742)
+        .longitude(8.49593039)
+        .discriminator("Eingang & Ausgang")
+        .parkID(1)
+        .build();
+
+    bugapointRepository.save(haupteingangLuisenpark);
+
+    Bugapoint swrStudio = Bugapoint.builder()
+        .id(2)
+        .adminID(1)
+        .title("SWR Studio")
+        .latitude(49.49613577872157)
+        .longitude(8.516308888163922)
+        .discriminator("Klimapark")
+        .parkID(2)
+        .build();
+
+    bugapointRepository.save(swrStudio);
   }
 
 
+  /**
+   * Tests all the insertions from setup()
+   */
   @Test
   void testInsertions() {
     Park luisenparkExpected = parkRepository.findById(1).get();
@@ -72,6 +106,12 @@ public class BaseTest {
 
     Admin expectSuperAdmin = adminRepository.findById(1).get();
     assertThat(expectSuperAdmin.getEmailadress()).isEqualTo("maxmustermann@buga23.com");
+
+    Bugapoint expectHaupteingangLuisenpark = bugapointRepository.findById(1).get();
+    assertThat(expectHaupteingangLuisenpark.getTitle()).isEqualTo("Haupteingang Luisenpark");
+
+    Bugapoint expectSwrStudio = bugapointRepository.findById(2).get();
+    assertThat(expectSwrStudio.getTitle()).isEqualTo("SWR Studio");
   }
 
 
